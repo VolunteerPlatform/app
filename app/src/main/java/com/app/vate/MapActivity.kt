5c6120c6f5af02306738b9f6230f1a63f24f16ae
han-gyeong <hangyeong@outlook.com>
2022-07-08 15:08:49 +0900

    feat: 현재 위치 확인 기능과 위치 마커 추가

--- app/src/main/java/com/app/vate/MapActivity.kt
+++ app/src/main/java/com/app/vate/MapActivity.kt
@@ -1,17 +1,24 @@
 package com.app.vate
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.app.vate.databinding.MapActivityBinding
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
 class MapActivity : AppCompatActivity() {
 
     private lateinit var binding: MapActivityBinding
     private var mapView : MapView? = null
+    private var currentLocationPOI: MapPOIItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +26,52 @@ class MapActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initMap()
+        initButton()
+    }
+
+    private fun initButton() {
+        binding.moveToCurrentLocationButton.setOnClickListener {
+            moveCenterToUserCurrentLocation()
+        }
     }
 
     private fun initMap() {
         mapView = MapView(this)
         binding.mapView.addView(mapView)
         checkRuntimePermission()
+
+        // 현재 위치 버튼을 맨 앞으로 가져오기 위해서
+        binding.moveToCurrentLocationButton.bringToFront()
+        moveCenterToUserCurrentLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun moveCenterToUserCurrentLocation() {
+        val lm: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val userCurrentLocation: Location? = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
+        val uLatitude = userCurrentLocation?.latitude
+        val uLongitude = userCurrentLocation?.longitude
+        val uCurrentPosition = MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!)
+
+        val marker = MapPOIItem()
+        marker.itemName = "Current Location"
+        marker.tag = 1
+        marker.mapPoint = uCurrentPosition
+        marker.markerType = MapPOIItem.MarkerType.CustomImage
+
+        marker.customImageResourceId = R.drawable.location
+        marker.isCustomImageAutoscale = false
+
+        // 현 위치가 옮겨질 경우 마커가 여러개 찍힐 수 있으므로
+        if (currentLocationPOI != null) {
+            mapView?.removePOIItem(currentLocationPOI)
+        }
+
+        mapView?.addPOIItem(marker)
+        currentLocationPOI = marker
+
+        mapView?.setMapCenterPointAndZoomLevel(uCurrentPosition, 2,true)
     }
 
     private fun checkRuntimePermission() {
