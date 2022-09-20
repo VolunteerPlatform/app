6720ec7a7cd08c0c5ef011f544ddc0f55c452975
이종수 <81428357+Lee-JongSoo@users.noreply.github.com>
2022-09-20 14:21:55 +0900

    Feat: 아이디 중복체크 기능 구현

--- app/src/main/java/com/app/vate/activity/SignupEmailActivity.kt
+++ app/src/main/java/com/app/vate/activity/SignupEmailActivity.kt
@@ -8,17 +8,68 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.app.vate.R
+import com.app.vate.api.LoginIdValidation
+import com.app.vate.api.PostResult
+import com.app.vate.api.Signup
 import com.app.vate.databinding.SignupActivityBinding
 import com.app.vate.databinding.SignupEmailActivityBinding
+import com.app.vate.model.LoginForm
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignupEmailActivity : AppCompatActivity() {
 
     private lateinit var binding: SignupEmailActivityBinding
+    val api = LoginIdValidation.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = SignupEmailActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var isValidation = false
+
+        binding.loginIdValidation.setOnClickListener {
+
+            val email = binding.editTextEmail.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(applicationContext, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+
+            } else {
+
+                val data = LoginForm(
+                    userName = email,
+                    password = ""
+                )
+
+                api.loginValidation(data).enqueue(object : Callback<PostResult> {
+                    override fun onResponse(
+                        call: Call<PostResult>,
+                        response: Response<PostResult>
+                    ) {
+                        if (response.isSuccessful) {
+
+                            Log.d("tlqkf", response.body()?.statusCode.toString())
+
+                            isValidation = if (response.body()?.statusCode == 200) {
+                                Toast.makeText(applicationContext, "회원 가입 가능한 아이디 입니다.", Toast.LENGTH_SHORT).show()
+                                true
+                            } else {
+                                Toast.makeText(applicationContext, "이미 가입된 회원입니다.", Toast.LENGTH_SHORT).show()
+                                false
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<PostResult>, t: Throwable) {
+                        Log.d("test signup", t.message.toString())
+                        Log.d("test signup", "fail login")
+                    }
+                })
+            }
+        }
 
         val button: Button = findViewById(R.id.signup)
         button.setOnClickListener {
@@ -31,11 +82,17 @@ class SignupEmailActivity : AppCompatActivity() {
                     Toast.makeText(applicationContext, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
                 }
                 !(email.contains('@')) || !(email.contains('.')) -> {
-                    Toast.makeText(applicationContext, "이메일 형식이 잘못 되었습니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "이메일 형식이 잘못 되었습니다.", Toast.LENGTH_SHORT)
+                        .show()
                 }
                 else -> {
                     passwordInfo.putExtra("email", email)
-                    startActivity(passwordInfo)
+
+                    if (isValidation) {
+                        startActivity(passwordInfo)
+                    } else {
+                        Toast.makeText(applicationContext, "아이디 중복 체크를 진행 후 회원가입 해주세요.", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/app/vate/api/LoginIdValidation.kt
@@ -0,0 +1,37 @@
+package com.app.vate.api
+
+import com.app.vate.model.LoginForm
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface LoginIdValidation {
+
+    @POST("/members/validation/loginId")
+    @Headers(
+        "accept: application/json",
+        "content-type: application/json"
+    )
+    fun loginValidation(
+        @Body jsonparams: LoginForm
+    ): Call<PostResult>
+
+    companion object { // static 처럼 공유객체로 사용가능함. 모든 인스턴스가 공유하는 객체로서 동작함.
+        private const val BASE_URL = "http://10.0.2.2:8080"
+        fun create(): LoginIdValidation {
+
+            val gson: Gson = GsonBuilder().setLenient().create();
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(LoginIdValidation::class.java)
+        }
+    }
+}
