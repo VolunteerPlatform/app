dfcb58b888a4f528046d00c4fc795ea456d3d0ed
han-gyeong <hangyeong@outlook.com>
2022-10-13 02:55:19 +0900

    Fix: 필요없는 Toast 메세지 삭제

--- app/src/main/java/com/app/vate/activity/MypageFragment.kt
+++ app/src/main/java/com/app/vate/activity/MypageFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.app.vate.R
 
@@ -27,7 +26,6 @@ class MypageFragment : Fragment() {
 
     private fun initButton(view: View) {
         view.findViewById<TextView>(R.id.myTimetable)?.setOnClickListener {
-            Toast.makeText(view?.context, "이름 선택되었음", Toast.LENGTH_SHORT).show()
             val intent = Intent(it.context, TimeTableActivity::class.java)
             startActivity(intent)
         }
