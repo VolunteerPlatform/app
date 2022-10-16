58caa5cbd71682aefb312908064fa9473b0bf67d
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-10-16 13:35:11 +0900

    edit 비밀번호 인증 시 회원 이름 및 비밀번호 추가 (#18)

--- app/src/main/java/com/app/vate/activity/FindPasswordActivity.kt
+++ app/src/main/java/com/app/vate/activity/FindPasswordActivity.kt
@@ -35,8 +35,10 @@ class FindPasswordActivity : AppCompatActivity() {
         binding.findPasswordButton.setOnClickListener {
             val editUserRealName: EditText = binding.enterUserId
             val userName = editUserRealName.text.toString()
+            val userRealName = binding.enterUserName.text.toString()
+            val userPhoneNumber = binding.enterUserPhoneNumber.text.toString()
 
-            val findPasswordForm = FindPasswordForm(userName)
+            val findPasswordForm = FindPasswordForm(userName, userRealName, userPhoneNumber)
 
             api.findPassword(findPasswordForm).enqueue(object : Callback<PostResult> {
                 override fun onResponse(call: Call<PostResult>, response: Response<PostResult>) {
--- app/src/main/java/com/app/vate/model/FindPasswordForm.kt
+++ app/src/main/java/com/app/vate/model/FindPasswordForm.kt
@@ -3,4 +3,6 @@ package com.app.vate.model
 data class FindPasswordForm(
 
     var userName: String = "",
+    var userRealName: String = "",
+    var phoneNumber: String = ""
 )
