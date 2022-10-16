aff9b920829b7e3eade636ec6d7a3ceb6bc6b494
han-gyeong <hangyeong@outlook.com>
2022-10-16 12:09:36 +0900

    Fix: 통합된 레트로핏 인스턴스 사용하도록 수정

--- app/src/main/java/com/app/vate/api/FindLoginId.kt
+++ app/src/main/java/com/app/vate/api/FindLoginId.kt
@@ -1,11 +1,7 @@
 package com.app.vate.api
 
 import com.app.vate.model.FindLoginIdForm
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -22,15 +18,9 @@ interface FindLoginId {
     ): Call<PostResult>
 
     companion object { // static 처럼 공유객체로 사용가능함. 모든 인스턴스가 공유하는 객체로서 동작함.
-        private const val BASE_URL = "http://10.0.2.2:8080"
         fun create(): FindLoginId {
-
-            val gson: Gson = GsonBuilder().setLenient().create();
-
-            return Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build()
+            return ApiClient
+                .getClient()
                 .create(FindLoginId::class.java)
         }
     }
--- app/src/main/java/com/app/vate/api/UpdateMemberProfile.kt
+++ app/src/main/java/com/app/vate/api/UpdateMemberProfile.kt
@@ -1,16 +1,9 @@
 package com.app.vate.api
 
-import com.app.vate.model.FindLoginIdForm
-import com.app.vate.model.FindPasswordForm
 import com.app.vate.model.UpdateMemberProfileForm
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.Headers
-import retrofit2.http.POST
 import retrofit2.http.PUT
 
 interface UpdateMemberProfile {
@@ -25,11 +18,7 @@ interface UpdateMemberProfile {
     ): Call<PostResult>
 
     companion object { // static 처럼 공유객체로 사용가능함. 모든 인스턴스가 공유하는 객체로서 동작함.
-        private const val BASE_URL = "http://10.0.2.2:8080"
         fun create(): UpdateMemberProfile {
-
-            val gson: Gson = GsonBuilder().setLenient().create();
-
             return ApiClient.getClient()
                 .create(UpdateMemberProfile::class.java)
         }
